3cf48bf19b4b5529274e825f2a409cb0e0821702
Pablocotal1 <pa.cotal@duocuc.cl>
2024-05-10 01:54:02 -0400

    delete

--- projects/urls.py
+++ projects/urls.py
@@ -1,9 +1,14 @@
-from rest_framework import routers
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .api import ProjectViewSet
 
-router = routers.DefaultRouter()
+# Definir el enrutador
+router = DefaultRouter()
+router.register('projects', ProjectViewSet)
 
-router.register('api/projects', ProjectViewSet, 'projects')
-
-urlpatterns = router.urls
+# Definir las URLs
+urlpatterns = [
+    # Incluir las URLs del enrutador
+    path('api/', include(router.urls)),
+]
 
